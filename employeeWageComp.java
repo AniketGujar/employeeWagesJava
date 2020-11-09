a305bd411d0d01209a7c0dd0515d6dbc5dec00d3
AniketGujar <gujaraniket7875@gmail.com>
2020-11-10 00:38:49 +0530

    Added: Daily Wage of employeee per hour

--- employeeWageComp.java
+++ employeeWageComp.java
@@ -1,11 +1,12 @@
 public class employeeWageComp {
 
 	public static void main(String[] args) {
-		int Full_Time = 1;
+		int full_Time = 1;
+		int full_Day_Wage = 160;
 		double empCheck = Math.floor(Math.random()*10)%2;
-		if (empCheck == Full_Time)
-			System.out.println("Employee is Present");
+		if (empCheck == full_Time)
+			System.out.println("Employee is Present, Daily Wage = " + full_Day_Wage);
 		else
-			System.out.println("Employee is Absent");
+			System.out.println("Employee is Absent, Daily Wage = 0");
 	}
 }
