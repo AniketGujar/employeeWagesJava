6a58c39b18b605364cc8ef4df458b87fbcc749a9
AniketGujar <gujaraniket7875@gmail.com>
2020-11-10 18:31:31 +0530

    Added:ConditionOfMaxHrsOrDays

--- employeeWageComp.java
+++ employeeWageComp.java
@@ -4,28 +4,38 @@ public class employeeWageComp {
 		int full_Time = 1;
 		int part_Time = 20;
 		int full_Day_Wage = 160;
-   for(int day=0; day<20; day++) {
+		int totalEmpHrs=0;
+		int totalWorkingDays=0;
+		int empHrs=0;
+
+while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
 
 		double empCheck = Math.floor(Math.random()*10)%3;
 		int partTimeCheck = (int)Math.floor(Math.random()*10)%4;
-
+		totalWorkingDays++;
 		if (empCheck == 0)
+			totalEmpHrs=totalEmpHrs+8;
 			System.out.println("Employee is Present, Daily Wage = " + full_Day_Wage);
 
-                else if (empCheck == 1)
+                else if (empCheck == 1) 
                         System.out.println("Employee is Absent, Daily Wage = 0");
 
 		else
 			switch (partTimeCheck) {
 			case 1:
+			empHrs=1;
 			System.out.println("Employee is Present Part Time, Daily Wage = " + part_Time);
                         case 2:
-                        System.out.println("Employee is Present Part Time, Daily Wage = " + part_Time*2);
+			empHrs=2;
+ 			System.out.println("Employee is Present Part Time, Daily Wage = " + part_Time*2);
                         case 3:
+			empHrs=3;
                         System.out.println("Employee is Present Part Time, Daily Wage = " + part_Time*3);
                         case 0:
+			empHrs=4;
                         System.out.println("Employee is Present Part Time, Daily Wage = " + part_Time*4);
 			}
-	}
+		}
+	totalEmpHrs = totalEmpHrs + empHrs;
    }
 }
