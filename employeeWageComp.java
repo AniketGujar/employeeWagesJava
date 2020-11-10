4625e3d243135fadf3c6f95d2f1be59bd9f6780d
AniketGujar <gujaraniket7875@gmail.com>
2020-11-10 18:02:02 +0530

    Added:SolvedUsingSwitchCase

--- employeeWageComp.java
+++ employeeWageComp.java
@@ -2,13 +2,18 @@ public class employeeWageComp {
 
 	public static void main(String[] args) {
 		int full_Time = 1;
-		int part_Time = 2;
+		int part_Time = 20;
 		int full_Day_Wage = 160;
 		double empCheck = Math.floor(Math.random()*10)%3;
-		int partTimeCheck =(int)Math.floor(Math.random()*10)%4;
+		int partTimeCheck = (int)Math.floor(Math.random()*10)%4;
+
 		if (empCheck == 0)
 			System.out.println("Employee is Present, Daily Wage = " + full_Day_Wage);
-		else if (empCheck == 1)
+
+                else if (empCheck == 1)
+                        System.out.println("Employee is Absent, Daily Wage = 0");
+
+		else
 			switch (partTimeCheck) {
 			case 1:
 			System.out.println("Employee is Present Part Time, Daily Wage = " + part_Time);
@@ -19,7 +24,5 @@ public class employeeWageComp {
                         case 0:
                         System.out.println("Employee is Present Part Time, Daily Wage = " + part_Time*4);
 			}
-		else
-			System.out.println("Employee is Absent, Daily Wage = 0");
 	}
 }
