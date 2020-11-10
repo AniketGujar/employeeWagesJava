4e7a74da66d481e588351b7d3f204d22d434c7be
AniketGujar <gujaraniket7875@gmail.com>
2020-11-10 17:52:38 +0530

    Added: Part Time Employee Wages

--- employeeWageComp.java
+++ employeeWageComp.java
@@ -2,10 +2,23 @@ public class employeeWageComp {
 
 	public static void main(String[] args) {
 		int full_Time = 1;
+		int part_Time = 2;
 		int full_Day_Wage = 160;
-		double empCheck = Math.floor(Math.random()*10)%2;
-		if (empCheck == full_Time)
+		double empCheck = Math.floor(Math.random()*10)%3;
+		int partTimeCheck =(int)Math.floor(Math.random()*10)%4;
+		if (empCheck == 0)
 			System.out.println("Employee is Present, Daily Wage = " + full_Day_Wage);
+		else if (empCheck == 1)
+			switch (partTimeCheck) {
+			case 1:
+			System.out.println("Employee is Present Part Time, Daily Wage = " + part_Time);
+                        case 2:
+                        System.out.println("Employee is Present Part Time, Daily Wage = " + part_Time*2);
+                        case 3:
+                        System.out.println("Employee is Present Part Time, Daily Wage = " + part_Time*3);
+                        case 0:
+                        System.out.println("Employee is Present Part Time, Daily Wage = " + part_Time*4);
+			}
 		else
 			System.out.println("Employee is Absent, Daily Wage = 0");
 	}
