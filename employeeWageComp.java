db343cdeeaad409e542568ab7724a28fc62a6724
AniketGujar <gujaraniket7875@gmail.com>
2020-11-11 18:07:18 +0530

    Added: Code Refactored By Adding OOPS Concept

--- employeeWageComp.java
+++ employeeWageComp.java
@@ -1,41 +1,59 @@
-public class employeeWageComp {
+class EmployeeWages{
 
-	public static void main(String[] args) {
-		int full_Time = 1;
-		int part_Time = 20;
-		int full_Day_Wage = 160;
-		int totalEmpHrs=0;
-		int totalWorkingDays=0;
-		int empHrs=0;
+        static final int DailyWage=160;
+        static final int HalfDayWage=80;
+        int TotalWage=0;
+        static final int HourlyWage=20;
+        int OverTimeWage;
 
-while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
 
-		double empCheck = Math.floor(Math.random()*10)%3;
-		int partTimeCheck = (int)Math.floor(Math.random()*10)%4;
-		totalWorkingDays++;
-		if (empCheck == 0)
-			totalEmpHrs=totalEmpHrs+8;
-			System.out.println("Employee is Present, Daily Wage = " + full_Day_Wage);
+        public void  monthlyWages(){
+                int OverTime;
+                for(int i=1; i<=20;i++){
 
-                else if (empCheck == 1) 
-                        System.out.println("Employee is Absent, Daily Wage = 0");
+                int Attendance = (int)Math.floor(Math.random()*10)%7;
+                int OverTimeHours = (int)Math.floor(Math.random()*10)%4;
 
-		else
-			switch (partTimeCheck) {
-			case 1:
-			empHrs=1;
-			System.out.println("Employee is Present Part Time, Daily Wage = " + part_Time);
-                        case 2:
-			empHrs=2;
- 			System.out.println("Employee is Present Part Time, Daily Wage = " + part_Time*2);
-                        case 3:
-			empHrs=3;
-                        System.out.println("Employee is Present Part Time, Daily Wage = " + part_Time*3);
-                        case 0:
-			empHrs=4;
-                        System.out.println("Employee is Present Part Time, Daily Wage = " + part_Time*4);
-			}
-		}
-	totalEmpHrs = totalEmpHrs + empHrs;
-   }
+        switch (Attendance){
+        case 1:
+                System.out.println( "Day        " + i   + "         00          "    +  TotalWage);
+
+        case 6:
+                TotalWage=TotalWage+HalfDayWage;
+                System.out.println( "Day        "+ i + "                "+ HalfDayWage + "              "+ TotalWage);
+
+        case 5:
+                switch (OverTimeHours){
+                case 4: OverTimeWage=(4*20);
+                TotalWage=TotalWage+OverTimeWage;
+                System.out.println( "Day        " + i  +"               "+ OverTimeWage + "           "  +  TotalWage);
+                break;
+                case 1:
+                OverTime=(3*20);
+                TotalWage=TotalWage+OverTimeWage;
+                System.out.println( "Day        " + i  +"               "+ OverTimeWage + "           "  +  TotalWage);
+                break;
+                case 2:
+                OverTime=(2*20);
+                TotalWage=TotalWage+OverTimeWage;
+                System.out.println( "Day        " + i  +"               "+ OverTimeWage  + "           " +  TotalWage);
+                break;
+                case 3:
+                OverTime=20;
+                TotalWage=TotalWage+OverTimeWage;
+                System.out.println( "Day        " + i  +"               "+OverTimeWage + "           "  +  TotalWage);
+                break;
+                }
+
+        default:
+                TotalWage=TotalWage+DailyWage;
+                System.out.println( "Day        " + i  + "           "+ DailyWage  + "           "+  TotalWage);
+              }
+        }
+        }
+  public static void main(String[] args) {
+    System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+    EmployeeWages obj = new EmployeeWages();
+    obj.monthlyWages();
+  }
 }
