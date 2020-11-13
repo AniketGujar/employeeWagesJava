d1ae9f2820a7a75ba2f95b456dc02378ae777e2e
AniketGujar <gujaraniket7875@gmail.com>
2020-11-13 17:35:02 +0530

    Added: Computation for multiple Companies

--- employeeWageComp.java
+++ employeeWageComp.java
@@ -1,14 +1,15 @@
-class EmployeeWages{
+class EmployeeWage{
 
-        static final int DailyWage=160;
-        static final int HalfDayWage=80;
-        int TotalWage=0;
-        static final int HourlyWage=20;
-        int OverTimeWage;
-
-
-        public void  monthlyWages(){
+        public void monthlyWages(String a,int dw,int hdw,int hw){
+                int DailyWage=dw;
+                int HalfDayWage=hdw;
+                int HourlyWage=hw;
+                int OverTimeWage=0;
                 int OverTime;
+                int TotalWage=0;
+                String CompanyName=a;
+
+                 System.out.println("Employee Wage Computatuion For "+ CompanyName + " Company");
                 for(int i=1; i<=20;i++){
 
                 int Attendance = (int)Math.floor(Math.random()*10)%7;
@@ -16,44 +17,49 @@ class EmployeeWages{
 
         switch (Attendance){
         case 1:
-                System.out.println( "Day        " + i   + "         00          "    +  TotalWage);
-
+                System.out.println( "Day " + i   + "\t\t00\t\t"    +  TotalWage);
+                break;
         case 6:
-                TotalWage=TotalWage+HalfDayWage;
-                System.out.println( "Day        "+ i + "                "+ HalfDayWage + "              "+ TotalWage);
-
+                TotalWage+=HalfDayWage;
+                System.out.println( "Day "+ i + "\t\t"+ HalfDayWage + "\t\t"+ TotalWage);
+                break;
         case 5:
                 switch (OverTimeHours){
                 case 4: OverTimeWage=(4*20);
-                TotalWage=TotalWage+OverTimeWage;
-                System.out.println( "Day        " + i  +"               "+ OverTimeWage + "           "  +  TotalWage);
+                TotalWage+=OverTimeWage;
+                System.out.println( "Day " + i  +"\t\t"+ OverTimeWage + "\t\t"  +  TotalWage);
                 break;
                 case 1:
                 OverTime=(3*20);
-                TotalWage=TotalWage+OverTimeWage;
-                System.out.println( "Day        " + i  +"               "+ OverTimeWage + "           "  +  TotalWage);
+                TotalWage+=OverTimeWage;
+                System.out.println( "Day " + i  +"\t\t"+ OverTimeWage + "\t\t"  +  TotalWage);
                 break;
                 case 2:
                 OverTime=(2*20);
-                TotalWage=TotalWage+OverTimeWage;
-                System.out.println( "Day        " + i  +"               "+ OverTimeWage  + "           " +  TotalWage);
+                TotalWage+=OverTimeWage;
+                System.out.println( "Day " + i  +"\t\t"+ OverTimeWage  + "\t\t" +  TotalWage);
                 break;
                 case 3:
                 OverTime=20;
-                TotalWage=TotalWage+OverTimeWage;
-                System.out.println( "Day        " + i  +"               "+OverTimeWage + "           "  +  TotalWage);
+                TotalWage+=OverTimeWage;
+                System.out.println( "Day " + i  +"\t\t"+OverTimeWage + "\t\t"  +  TotalWage);
                 break;
                 }
-
+                break;
         default:
                 TotalWage=TotalWage+DailyWage;
-                System.out.println( "Day        " + i  + "           "+ DailyWage  + "           "+  TotalWage);
-              }
+                System.out.println( "Day " + i  + "\t\t"+ DailyWage  + "\t\t"+  TotalWage);
+                break;
+                }
         }
         }
   public static void main(String[] args) {
-    System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
-    EmployeeWages obj = new EmployeeWages();
-    obj.monthlyWages();
+    System.out.println("Welcome to Employee Wage Computation Program");
+         EmployeeWage c1 = new EmployeeWage();
+         c1.monthlyWages("Wipro",160,80,20);
+         EmployeeWage c2 = new EmployeeWage();
+         c2.monthlyWages("TCS",170,70,35);
+         EmployeeWage c3 = new EmployeeWage();
+         c3.monthlyWages("LNT",190,90,35);
   }
 }
